71b28e713cc32d1897e05eaa6d3f6f99fa389347
Yu Jie Teo <yujie@protonmail.com>
2025-10-20 16:35:25 +0800

    feat: add blog post

---
+++ trial/pages/047-251020_finds.c
@@ -0,0 +1,21 @@
+/*Karpathy's ChatGPT prompts: be straightforward and get to the point. I love learning, education, explanations and insights.*/
+/*Taleb on decay in tail risk hedging: convexity is equal decay, you need precision. The devil is in the heuristics.*/
+/*Taleb on reasons to buy options: if you have any reasons to buy an option, for speculation only buy options when you have no reason to buy them. If there is a reason to buy it, it will be priced in.*/
+/*Taleb on Schwartz distributions and generalised functions: theory of distributions can be simplified with an analogy to option pricing.*/
+/*Taleb on nonergodicity of selling: no option seller survives this is because selling is nonergodic.*/
+/*Tenobrus on performance impact of copies: performance impact on copies in C++ is very bad, removing full collection copies can improve a production codebase by 100%. Always turn copies into reference address.*/
+/*<code>sizeof()</code> misconception: <code>sizeof(short)</code> is not the number of bits in a short.*/
+/*Wrong ladder wall analogy in reading papers: most important thing is why am I reading this paper and what am I trying to learn and how long should I spent learning this. Focusing the scope save times because you are optimising the quality and value for effort. Sort papers into title, authors and keywords | abstracts and references | papers with key proofs or techniques. | entire papers | papers I can reproduce myself.*/
+/*Math paper reading is nonlinear: one year to read one paper in intersection theory, constantly question, cross examine, take notes.*/
+/*Numbers every computer architecture designer should know: examples include L1 cache reference, branch misprediction, mutex lock and unlock, these are used for back of the envelope calculation for a system design.*/
+/*Topics in software development learn possibly: SQL, DB, Kafka/Redis/Docker, Linux, C, JS/HTML/CSS.*/
+/*Spec R[x] as C modulo conjugation: it is a good idea that for any field k, the closed points of the affine line over k in dimension n is in bijection with the Galois orbits of the algebraic closure of k. Example, real valued polynomials have pairs of conjugate roots or purely real roots subsumed as one root. So Spec R[x] is Spec C modulo conjugation.*/
+/*Neukrich perspective on number fields, by Qiaochu Yuan: rings of integers that are made algebraically closed have their singularities resolved.*/
+/*Schur's lemman, one line proof: every endomorphism has an eigenvalue over the field and eigenspaces as subrepresentations.*/
+/*Poonen's analogies in number fields and function fields: deep analogy here for Arakelov theory, look for the table by him.*/
+/*George Hotz on compilers: compilers are not good enough to match English coding. Prompts are just bad abstractions.*/
+/*System design algorithms: surprise! geohash, quadtree, consistent hashing load balancer, rate limiter leaky token buckets, tries, rsync, Raft, bloom filter, Merkle tree, hyperloglog, count-min sketch, hierarchial timing wheels job scheduler, operational transformation for collaborative editing.*/
+/*Focusing and time: focus more of your time on what you want to see more of.
+*/
+/*Information abundance leads to distortion: under 30 prematurely old worried about savings career and FIRE, over 50 is desperately young. Missed experiences.*/
+/*Karpathy on blog posts and slides: don't write blogs, don't do slides, build the code, arrange it, get it to work.*/
